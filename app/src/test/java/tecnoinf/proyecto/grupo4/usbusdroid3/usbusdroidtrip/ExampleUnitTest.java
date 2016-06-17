bbf3874f208c05967c181f11372848c5df19e3f8
kavesa.laptop <kavesa@gmail.com>
2016-06-16 21:33:19 -0300

    Estructura

---
+++ app/src/test/java/tecnoinf/proyecto/grupo4/usbusdroid3/usbusdroidtrip/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package tecnoinf.proyecto.grupo4.usbusdroid3.usbusdroidtrip;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
